bf5d67a587f5460a8de42038ea15d44caea22e73
rafael <rafareisbr@gmail.com>
2018-11-20 21:40:11 -0200

    Update Crud Localization

--- invscp/src/main/java/br/ufg/invscp/controller/LocalizacaoController.java
+++ invscp/src/main/java/br/ufg/invscp/controller/LocalizacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +30,16 @@ public class LocalizacaoController {
 	public ModelAndView novaLocalizacao(Model model) {
 		model.addAttribute("localizacao", new Localization());
 		return new ModelAndView("paginas/localizacao/new");
+	}
+
+
+	// Não sei se funciona, nem edit nem delete.
+	@RequestMapping("/edit/{id}")
+	public ModelAndView editLocalizacao(@PathVariable Long id, Model model) {
+		Localization localization = localizationService.findLocalization(id);
+		System.out.println(localization);
+		model.addAttribute("localization", localization);
+		return new ModelAndView("paginas/localizacao/edit");
 	}
 	
 	@RequestMapping("/")
