86a8e3387d8c8fb6a0b4a615c6d2e149585d2fe1
Erivan <erivan.nas@gmail.com>
2018-12-04 17:34:37 -0200

    Departamento

--- invscp/src/main/java/br/ufg/invscp/controller/DepartamentoController.java
+++ invscp/src/main/java/br/ufg/invscp/controller/DepartamentoController.java
@@ -9,12 +9,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.ufg.invscp.model.Departamento;
 import br.ufg.invscp.model.Localization;
 import br.ufg.invscp.model.Predio;
+import br.ufg.invscp.model.Sala;
+import br.ufg.invscp.model.dto.DepartamentoDTO;
 import br.ufg.invscp.model.dto.LocalizationDTO;
 import br.ufg.invscp.model.dto.PredioDTO;
+import br.ufg.invscp.model.dto.SalaDTO;
+import br.ufg.invscp.repository.UsuarioRepository;
+import br.ufg.invscp.service.DepartamentoService;
 import br.ufg.invscp.service.LocalizationService;
 import br.ufg.invscp.service.PredioService;
+import br.ufg.invscp.service.SalaService;
 
 @Controller
 @RequestMapping("departamento")
@@ -26,37 +33,46 @@ public class DepartamentoController {
 	@Autowired 
 	private PredioService predioService;
 	
+	@Autowired
+	private SalaService salaService;
+	
+	@Autowired
+	private DepartamentoService departamentoService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	@PostMapping("/create")
-	public ModelAndView save(PredioDTO  predio, Model model) {
-		predioService.save(predio);
+	public ModelAndView save(DepartamentoDTO dto, Model model) {
+		departamentoService.save(dto);
 		return findAll(model);
 	}
 	
 	@RequestMapping("/new")
-	public ModelAndView novoPredio(Model model) {
-		model.addAttribute("predio", new PredioDTO());
-		model.addAttribute("localizacoes", localizationService.findAllLocalizations());
-		return new ModelAndView("paginas/predio/new");
+	public ModelAndView novoDepartamento(Model model) {
+		model.addAttribute("departamento", new DepartamentoDTO());
+		model.addAttribute("usuarios", usuarioRepository.findAll());
+		return new ModelAndView("paginas/departamento/departamento-new");
 	}
 	
 	@RequestMapping("/edit/{id}")
 	public ModelAndView editPredio(@PathVariable Long id, Model model) {
-		Predio predio = predioService.find(id);
-		model.addAttribute("predio", predio);
-		model.addAttribute("localizacoes", localizationService.findAllLocalizations());
-		return new ModelAndView("paginas/predio/edit");
+		DepartamentoDTO departamento = departamentoService.findDTO(id);
+		model.addAttribute("departamento", departamento);
+		model.addAttribute("usuarios", usuarioRepository.findAll());
+		return new ModelAndView("paginas/departamento/departamento-edit");
 	}
 	
 	@PostMapping("/update/{id}")
-	public ModelAndView update(@PathVariable Long id, Predio  predio, Model model) {
-		predioService.update(id, predio);
+	public ModelAndView update(@PathVariable Long id, DepartamentoDTO dto, Model model) {
+		departamentoService.update(id, dto);
 		return findAll(model);
 	}
 	
 	@RequestMapping("/")
 	public ModelAndView findAll(Model model ) {
-		List<Predio> predios = predioService.findAll();
-		model.addAttribute("predios", predios);
-		return new ModelAndView("paginas/predio/list");
+		List<Departamento> departamentos = departamentoService.findAll();
+		model.addAttribute("departamentos", departamentos);
+		return new ModelAndView("paginas/departamento/departamento-list");
 	}
 }
